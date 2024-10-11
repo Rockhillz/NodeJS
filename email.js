a8334788713ae2451d8976757be8402de388eb6d
Rockhillz <izuchi.alaneme@gmail.com>
2024-10-11 19:53:45 +0100

    second commit

--- email.js
+++ email.js
@@ -5,15 +5,15 @@ const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
         user: 'izuchi.alaneme@gmail.com',
-        pass: '4961 995'
+        pass: 'fjzp fspn cprx wwqu'
     }
 });
 
 const mailOptions = {
     from: 'izuchi.alaneme@gmail.com',
-    to: 'izuchukwualaneme@gmail.com',
-    subject: 'Sending Email using Node.js',
-    text: 'That was easy!'
+    to: 'izuchukwualaneme@gmail.com, husseinimudiking@gmail.com, belloayoola20@gmail.com',
+    subject: 'Sending Email using Node.js From IzuCodes',
+    text: 'It was not easy at first. But finally found my APP PASSWORD. So This is my Mail using Node Js 😍😍👌!'
 };
 
 transporter.sendMail(mailOptions, (error, info) => {
